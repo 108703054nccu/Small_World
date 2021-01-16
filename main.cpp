962cf85ea8ff579a1382a8a50faf899bc2152ee4
sargisiton <108703054@g.nccu.edu.tw>
2021-01-16 15:16:04 +0800

    Small_world 1.01 unfinished line test

--- main.cpp
+++ main.cpp
@@ -5,11 +5,12 @@ void displayMe(void)
 	//graphic g;
 	//g.ShowGraphic();;
 	glClear(GL_COLOR_BUFFER_BIT);
-	glBegin(GL_POLYGON);
-	glVertex3f(0.5, 0.0, 0.5);
-	glVertex3f(0.5, 0.0, 0.0);
-	glVertex3f(0.0, 0.5, 0.0);
-	glVertex3f(0.0, 0.0, 0.5);
+	glColor3f(1.0,0.0,0.0);
+	glBegin(GL_LINES);
+	glVertex3f(0.0, 0.0, 0.0);
+	glVertex3f(15.0, 0.0, 0.0);
+	//glVertex3f(0.0, 0.5, 0.0);
+	//glVertex3f(0.0, 0.0, 0.5);
 	glEnd();
 	glFlush();
 }
