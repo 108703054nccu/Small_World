3dc38499b99ad4096374690cbf4f59333779cc2a
sargisiton <108703054@g.nccu.edu.tw>
2021-01-16 15:12:48 +0800

    Small_World 1.01 test

--- main.cpp
+++ main.cpp
@@ -2,15 +2,22 @@
 #include "Graphic.h"
 void displayMe(void)
 {
-	graphic g;
-	g.ShowGraphic();
+	//graphic g;
+	//g.ShowGraphic();;
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_POLYGON);
+	glVertex3f(0.5, 0.0, 0.5);
+	glVertex3f(0.5, 0.0, 0.0);
+	glVertex3f(0.0, 0.5, 0.0);
+	glVertex3f(0.0, 0.0, 0.5);
+	glEnd();
 	glFlush();
 }
 int main(int argc, char** argv){
 	graphic g;
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE);
-	glutInitWindowSize(1000, 800);
+	glutInitWindowSize(400, 300);
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("Draw_Result");
 	glutDisplayFunc(displayMe);
